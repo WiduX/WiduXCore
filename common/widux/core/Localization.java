0cde1d495a93ad0e61b20ef5ce9bb9fff0604c37
WiduX <WiduX@live.com>
2013-03-31 11:46:35 -0700

    Overhaul of Localization. Oops, this was supposed to committed a while ago.

--- common/widux/core/Localization.java
+++ common/widux/core/Localization.java
@@ -1,9 +1,9 @@
 package widux.core;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.security.InvalidParameterException;
+import java.util.LinkedList;
 import java.util.Properties;
-
 import net.minecraft.util.StringTranslate;
 
 /**
@@ -15,72 +15,122 @@ import net.minecraft.util.StringTranslate;
  */
 public class Localization
 {
-	private static final String DEFAULT_LANGUAGE = "en_US";
-	private String modName = null;
-	private String loadedLanguage = null;
-	private Properties defaultMappings = new Properties();
-	private Properties mappings = new Properties();
-
-	/**
-	 * Loads the mod's localization files. All language files must be stored in
-	 * "[modname]/lang/", in .properties files. (ex: for the mod 'invtweaks',
-	 * the french translation is in: "invtweaks/lang/fr_FR.properties")
-	 *
-	 * @param modName The mod name
-	 */
-	public Localization(String modName)
+	private static class modInfo
 	{
-		if (modName == null)
+		final String modName, defaultLanguage;
+		
+		public modInfo(String modName, String defaultLanguage)
 		{
-			throw new InvalidParameterException("Mod name can't be null");
+			this.modName = modName;
+			this.defaultLanguage = defaultLanguage;
 		}
-		this.modName = modName;
-		load(getCurrentLanguage());
 	}
-
+	
+	private static String loadedLanguage = getCurrentLanguage();
+	private static Properties defaultMappings = new Properties();
+	private static Properties mappings = new Properties();
+	private static LinkedList<modInfo> mods = new LinkedList<modInfo>();
+	
+	/**
+	 * Adds localization from a given directory. The files must have the same name as the corresponding language file in minecraft and a ".properties" file
+	 * extention e.g "en_US.properties"
+	 * 
+	 * @param path The path to the localization files
+	 * @param defaultLanguage The default localization to be used when there is no localization for the selected language or if a string is missing (e.g. "en_US")
+	 */
+	public static void addLocalization(String path, String defaultLanguage)
+	{
+		mods.add(new modInfo(path, defaultLanguage));
+		load(path, defaultLanguage);
+	}
+	
 	/**
 	 * Get a string for the given key, in the currently active translation.
-	 *
+	 * 
 	 * @param key
 	 * @return
 	 */
-	public synchronized String get(String key)
+	public static synchronized String get(String key)
 	{
-		String currentLanguage = getCurrentLanguage();
-		if (!currentLanguage.equals(loadedLanguage))
+		if (getCurrentLanguage() == null)
 		{
-			load(currentLanguage);
+			return key;
 		}
+		if (!getCurrentLanguage().equals(loadedLanguage))
+		{
+			defaultMappings.clear();
+			mappings.clear();
+			for (modInfo mInfo : mods)
+			{
+				load(mInfo.modName, mInfo.defaultLanguage);
+			}
+			loadedLanguage = getCurrentLanguage();
+		}
+		
 		return mappings.getProperty(key, defaultMappings.getProperty(key, key));
 	}
-
-	private void load(String newLanguage)
+	
+	private static void load(String path, String default_language)
 	{
-		defaultMappings.clear();
-		mappings.clear();
+		InputStream langStream = null;
+		Properties modMappings = new Properties();
+		
 		try
 		{
-			InputStream langStream = Localization.class.getResourceAsStream(modName + "/lang/" + newLanguage + ".properties");
-			InputStream defaultLangStream = Localization.class.getResourceAsStream(modName + "/lang/" + DEFAULT_LANGUAGE + ".properties");
-			mappings.load((langStream == null) ? defaultLangStream : langStream);
-			defaultMappings.load(defaultLangStream);
-
+			// Load the default language mappings
+			langStream = Localization.class.getResourceAsStream(path + default_language + ".properties");
+			modMappings.load(langStream);
+			defaultMappings.putAll(modMappings);
+			langStream.close();
+			
+			// Try to load the current language mappings.
+			// If the file doesn't exist use the default mappings.
+			langStream = Localization.class.getResourceAsStream(path + getCurrentLanguage() + ".properties");
 			if (langStream != null)
 			{
-				langStream.close();
+				modMappings.clear();
+				modMappings.load(langStream);
 			}
-			defaultLangStream.close();
-		}
-		catch (Exception e)
+			
+			// If the selected language inherits mappings from another language
+			// we use those first and then we overwrite the common ones with
+			// those in the selected language
+			if (modMappings.containsKey("language.parent")) {
+				langStream = Localization.class.getResourceAsStream(path + modMappings.get("language.parent") + ".properties");
+				
+				if (langStream != null)
+				{
+					Properties parentModMappings = new Properties();
+					
+					parentModMappings.load(langStream);
+					mappings.putAll(parentModMappings);
+				}
+			}
+			
+			mappings.putAll(modMappings);
+			
+		} catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		loadedLanguage = newLanguage;
+		finally
+		{
+			try
+			{
+				if (langStream != null)
+				{
+					langStream.close();
+				}
+			} catch (IOException ex)
+			{
+				ex.printStackTrace();
+			}
+		}
 	}
-
+	
 	private static String getCurrentLanguage()
 	{
 		return StringTranslate.getInstance().getCurrentLanguage();
 	}
-
+	
 }
